32cb75b60e08036ff4fff182ea3387c04ad3e06a
gp5dbo65 <daveotis65@gmail.com>
2021-06-24 23:26:10 -0400

    Update TeslaSalesReportApplication.java

--- src/com/coderscampus/assignment6/TeslaSalesReportApplication.java
+++ src/com/coderscampus/assignment6/TeslaSalesReportApplication.java
@@ -27,32 +27,14 @@ public class TeslaSalesReportApplication {
 		TeslaModelS = (teslaFS.loadSalesDataList("./modelS.csv"));
 		TeslaModelX = (teslaFS.loadSalesDataList("./modelX.csv"));
 	
-		/* create sales data report by model ***NON-STREAM SOLUTION*** */
-		ProcessSalesData(TeslaModel3, "Model 3");
-		System.out.println(REPORT_LINE_DIVIDER);
-		ProcessSalesData(TeslaModelS, "Model S");
-		System.out.println(REPORT_LINE_DIVIDER);
-		ProcessSalesData(TeslaModelX, "Model X");
-
-		/* create sales data report by model ***STREAM SOLUTION - INCOMPLETE*** */
+		/* create sales data report by model ***STREAM SOLUTION*** */
 		CreateSalesDataReport(TeslaModel3, "Model 3");
-		System.out.println(REPORT_LINE_DIVIDER);
-		
-//		CreateSalesDataReport(TeslaModel3, "Model 3");
-//		System.out.println(REPORT_LINE_DIVIDER);
-//
-//		CreateSalesDataReport(TeslaModel3, "Model 3");
-//		System.out.println(REPORT_LINE_DIVIDER);
+		CreateSalesDataReport(TeslaModelS, "Model S");
+		CreateSalesDataReport(TeslaModelX, "Model X");
 
 	} //end of main
 	
 	/* Sales Data Report Output */
-	/********************************************************************/
-	/* THIS METHOD WAS SUPPOSE TO USE STREAMS TO BUILD THE SALES REPORT */
-	/* I WAS UNABLE TO COMPLETE THIS TASK. I COULDN'T FIGURE OUT HOW TO */
-	/* CAPTURE AND PROCESS THE STREAM THAT WAS COMING OUT OF THE VALUE  */
-	/* PORTION OF THE HASH MAP DATA STRUCTURE groupedByYearlySalesData  */
-	/********************************************************************/
 	private static void CreateSalesDataReport(List<SalesData> teslaSalesData, String modelName) {
 		System.out.println(modelName + REPORT_HEADER);
 		System.out.println(REPORT_LINE_DIVIDER);
@@ -62,119 +44,32 @@ public class TeslaSalesReportApplication {
 				teslaSalesData.stream()
 				     		  .collect(Collectors.groupingBy(x -> x.getDate().getYear()));
 		
-		/* test code to view the contents of the hash map*/
-		System.out.println(groupedByYearlySalesData);
-		
-		/* Stuck on this section of the code. */
-		List<String> yearlySalesTotals = groupedByYearlySalesData.entrySet()
+		/* This block of code was done with Trevor's help. */
+		groupedByYearlySalesData.entrySet()
 								.stream()
-								.map(entry -> entry.getKey() + " -> " + 
-											  entry.getValue().stream() + "\n")
-//								.collect(Collectors.??????)
-//								.flatMapToInt(????)
-//								.map(entry -> getSales())
-								/* This .collect method was used to capture the results. */
-								/* Not sure what would be the final variable type        */
-								/* assignment should be for this portion of the stream   */
-								.collect(Collectors.toList());
-								
-		System.out.println(yearlySalesTotals);
+								.forEach(entry -> {
+									System.out.println(entry.getKey() + " -> " + 
+														entry.getValue()
+														.stream()
+														.mapToInt(salesData -> salesData.getSales())
+														.sum());
+														});
+		System.out.println();	
 		
+		/* use a sorted List of sales data to place the minimum sales total   */
+		/* at the front of the List and the maximum sales totals at the end   */
+		/* of the List. This block of code was done with Trevor's help!       */
+		List<SalesData> sortedTeslaSalesData = teslaSalesData.stream()
+									   .sorted((o1, o2) -> o1.getSales().compareTo(o2.getSales()))
+									   .collect(Collectors.toList());
 		
-		
-		
-		System.out.println(REPORT_LINE_DIVIDER);
-		
-		/* This was test code to see if I could find the min/max in the stream. */
-		/* However, this code cannot provide the YearMonth that is associated   */
-		/* with the min/max sales data. I've spent too much time trying to      */
-		/* get the yearly totals, that I just gave up on working with streams   */
-		OptionalInt maxSalesAmount = teslaSalesData.stream()
-												   .mapToInt(x -> x.getSales())
-												   .max();
-		
-		OptionalInt minSalesAmount = teslaSalesData.stream()
-//												   .mapToInt(x -> x.getDate())
-												   .mapToInt(x -> x.getSales())
-												   .min();
-		
-		System.out.println("Tesla Model 3 max sales = " + maxSalesAmount);
-		System.out.println("Tesla Model 3 min sales = " + minSalesAmount);
-
-		System.out.println();
-		
-	} //end of CreateSalesDataReport
-
-	/* Sales Data Report Output */
-	/********************************************************************/
-	/* I CREATED THIS METHOD TO BE USED TO VERIFY THE RESULTS OF THE    */
-	/* STREAM METHOD - CreateSalesDataReport - WHICH I WAS NOT ABLE TO  */
-	/* COMPLETE. I HAVE "HARD-CODED" FOUR YEAR SPECIFIC TOTAL FIELDS,   */
-	/* SO, IF THERE WERE ANY NEW DATE DATA ADDED TO THE CSV FILES, THEN */
-	/* THE PROGRAM WOULD TERMINATE AND DISPLAY A MESSAGE THAT THE YEAR- */
-	/* MONTH DATE ENCOUNTERED THAT WAS OUT OF BOUNDS (2016-2019)        */
-	/* A BETTER METHOD WOULD BE IF A HASH MAP WAS USED TO TRACK THE     */
-	/* YEARS AS THE KEY AND STORE A LIST OF SALESDATA AS THE VALUE      */
-	/********************************************************************/
-	private static void ProcessSalesData(List<SalesData> teslaSalesData, String modelName) {
-		Integer minSalesAmount = Integer.MAX_VALUE;
-		Integer maxSalesAmount = Integer.MIN_VALUE;
-		YearMonth minSalesYearMonth = null;
-		YearMonth maxSalesYearMonth = null;
-		Integer total2016Sales = 0;
-		Integer total2017Sales = 0;
-		Integer total2018Sales = 0;
-		Integer total2019Sales = 0;
-		Integer currentSalesAmount = 0;
-		
-		for(SalesData model : teslaSalesData) {
-			currentSalesAmount = model.getSales();
-			//minimum sales amount logic
-			if(currentSalesAmount < minSalesAmount) {
-				minSalesAmount = currentSalesAmount;
-				minSalesYearMonth = model.getDate();
-			} //end of minimum sales amount logic
-			//maximum sales amount logic
-			
-			if(currentSalesAmount > maxSalesAmount) {
-				maxSalesAmount = currentSalesAmount;
-				maxSalesYearMonth = model.getDate();
-			} //end of maximum sales amount logic
-			
-			//accumulate sales totals by year 2016 - 2019
-			switch (model.getDate().getYear()) {
-				case 2016:
-					total2016Sales += currentSalesAmount;
-					break;
-				case 2017:
-					total2017Sales += currentSalesAmount;
-					break;
-				case 2018:
-					total2018Sales += currentSalesAmount;
-					break;
-				case 2019:
-					total2019Sales += currentSalesAmount;
-					break;
-				default: //encountered YearMonth data that is out of bounds - abend program
-					System.out.println("Year model out of bounds");
-					System.out.println(model);
-					System.exit(999);
-			} //end switch block
-		} //end for-each block
-		
-		/* Sales Data Report Output */
-		System.out.println(modelName + REPORT_HEADER);
-		System.out.println(REPORT_LINE_DIVIDER);
-		//check to see if there are any sales data for 2016
-		if (total2016Sales > 0)
-			System.out.println("2016 -> " + total2016Sales);
-		System.out.println("2017 -> " + total2017Sales);
-		System.out.println("2018 -> " + total2018Sales);
-		System.out.println("2019 -> " + total2019Sales);
-		System.out.println();
+		YearMonth minSalesYearMonth = sortedTeslaSalesData.get(0).getDate();
+		YearMonth maxSalesYearMonth = sortedTeslaSalesData.get(sortedTeslaSalesData.size()-1).getDate();
+				
 		System.out.println("The best month for " + modelName + " was: " + maxSalesYearMonth);
 		System.out.println("The worst month for " + modelName + " was: " + minSalesYearMonth);
 		System.out.println();
-	} //end of ProcessSalesData method
+		
+	} //end of CreateSalesDataReport
 
 } //end of TeslaSalesReportApplication class
